c9c804febde3f06b1263daa3ee7eb8a8899ba6a3
Gilson Varghese <gilsonvarghese7@gmail.com>
2018-05-22 15:26:55 +0000

    Updated cost calculation

--- machine_learning_stanford/machine-learning-ex1/ex1/computeCost.m
+++ machine_learning_stanford/machine-learning-ex1/ex1/computeCost.m
@@ -13,8 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
+msre = X*theta - y 
+J = (msre' * msre) /(2*m)
 
 
 % =========================================================================
